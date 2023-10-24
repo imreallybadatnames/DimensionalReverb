9d958dece3d6fdadf35bb8edd9c442c1b4310a25
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-10-24 12:47:20 +0200

    pull codec key

--- src/main/java/de/dafuqs/reverb/sound/distortion/DistortionEffect.java
+++ src/main/java/de/dafuqs/reverb/sound/distortion/DistortionEffect.java
@@ -14,30 +14,30 @@ import java.util.function.*;
  * A Distortion effect controls
  */
 public abstract class DistortionEffect {
-
-    @SuppressWarnings("unchecked")
-    public static final Registry<Codec<? extends DistortionEffect>> DISTORTION_EFFECT_CODEC = (SimpleRegistry<Codec<? extends DistortionEffect>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier(Reverb.MOD_ID, "distortion_effect")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-    public static final Codec<DistortionEffect> CODEC = DISTORTION_EFFECT_CODEC.getCodec().dispatchStable(DistortionEffect::getCodec, Function.identity());
-
-    public abstract Codec<? extends DistortionEffect> getCodec();
-
-    /**
-     * Whether a Sound Event should be ignored
-     *
-     * @param identifier the Identifier of the Sound Event
-     */
-    public abstract boolean shouldIgnore(Identifier identifier);
-
-    public abstract boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getEdge(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getGain(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getLowpassCutoff(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getEQCenter(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getEQBandWidth(MinecraftClient client, SoundInstance soundInstance);
-
+	
+	public static final RegistryKey<Registry<Codec<? extends DistortionEffect>>> DISTORTION_EFFECT_CODEC_KEY = RegistryKey.ofRegistry(new Identifier(Reverb.MOD_ID, "distortion_effect"));
+	public static final Registry<Codec<? extends DistortionEffect>> DISTORTION_EFFECT_CODEC = FabricRegistryBuilder.createSimple(DISTORTION_EFFECT_CODEC_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+	public static final Codec<DistortionEffect> CODEC = DISTORTION_EFFECT_CODEC.getCodec().dispatchStable(DistortionEffect::getCodec, Function.identity());
+	
+	public abstract Codec<? extends DistortionEffect> getCodec();
+	
+	/**
+	 * Whether a Sound Event should be ignored
+	 *
+	 * @param identifier the Identifier of the Sound Event
+	 */
+	public abstract boolean shouldIgnore(Identifier identifier);
+	
+	public abstract boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getEdge(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getGain(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getLowpassCutoff(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getEQCenter(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getEQBandWidth(MinecraftClient client, SoundInstance soundInstance);
+	
 }
--- src/main/java/de/dafuqs/reverb/sound/reverb/ReverbEffect.java
+++ src/main/java/de/dafuqs/reverb/sound/reverb/ReverbEffect.java
@@ -14,44 +14,44 @@ import java.util.function.*;
  * A Reverb effect controls
  */
 public abstract class ReverbEffect {
-
-    @SuppressWarnings("unchecked")
-    public static final SimpleRegistry<Codec<? extends ReverbEffect>> REVERB_EFFECT_CODEC = (SimpleRegistry<Codec<? extends ReverbEffect>>) (Object) FabricRegistryBuilder.createSimple(Codec.class, new Identifier(Reverb.MOD_ID, "reverb_effect")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-    public static final Codec<ReverbEffect> CODEC = REVERB_EFFECT_CODEC.getCodec().dispatchStable(ReverbEffect::getCodec, Function.identity());
-
-    public abstract Codec<? extends ReverbEffect> getCodec();
-
-    /**
-     * Whether a Sound Event should be ignored
-     *
-     * @param identifier the Identifier of the Sound Event
-     */
-    public abstract boolean shouldIgnore(Identifier identifier);
-
-    public abstract boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getAirAbsorptionGainHF(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getDecayHFRatio(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getDensity(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getDiffusion(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getGain(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getGainHF(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getLateReverbGainBase(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getDecayTime(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getReflectionsGainBase(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract int getDecayHFLimit(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getLateReverbDelay(MinecraftClient client, SoundInstance soundInstance);
-
-    public abstract float getReflectionsDelay(MinecraftClient client, SoundInstance soundInstance);
-
+	
+	public static final RegistryKey<Registry<Codec<? extends ReverbEffect>>> REVERB_EFFECT_CODEC_KEY = RegistryKey.ofRegistry(new Identifier(Reverb.MOD_ID, "reverb_effect"));
+	public static final SimpleRegistry<Codec<? extends ReverbEffect>> REVERB_EFFECT_CODEC = FabricRegistryBuilder.createSimple(REVERB_EFFECT_CODEC_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+	public static final Codec<ReverbEffect> CODEC = REVERB_EFFECT_CODEC.getCodec().dispatchStable(ReverbEffect::getCodec, Function.identity());
+	
+	public abstract Codec<? extends ReverbEffect> getCodec();
+	
+	/**
+	 * Whether a Sound Event should be ignored
+	 *
+	 * @param identifier the Identifier of the Sound Event
+	 */
+	public abstract boolean shouldIgnore(Identifier identifier);
+	
+	public abstract boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getAirAbsorptionGainHF(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getDecayHFRatio(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getDensity(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getDiffusion(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getGain(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getGainHF(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getLateReverbGainBase(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getDecayTime(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getReflectionsGainBase(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract int getDecayHFLimit(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getLateReverbDelay(MinecraftClient client, SoundInstance soundInstance);
+	
+	public abstract float getReflectionsDelay(MinecraftClient client, SoundInstance soundInstance);
+	
 }
