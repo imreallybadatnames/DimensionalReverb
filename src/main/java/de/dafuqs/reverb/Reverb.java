21e276c9b09383d567df8ed4e5951137dabfa8e1
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-10-24 13:03:59 +0200

    remove test reverb

--- src/main/java/de/dafuqs/reverb/Reverb.java
+++ src/main/java/de/dafuqs/reverb/Reverb.java
@@ -8,8 +8,6 @@ import net.fabricmc.fabric.api.event.registry.*;
 import net.minecraft.registry.*;
 import net.minecraft.util.*;
 
-import java.util.*;
-
 public class Reverb implements ModInitializer {
 	
 	public static final String MOD_ID = "reverb";
@@ -21,12 +19,6 @@ public class Reverb implements ModInitializer {
 	public void onInitialize() {
 		Registry.register(ReverbEffect.REVERB_EFFECT_CODEC, new Identifier(MOD_ID, "static"), StaticReverbEffect.CODEC);
 		Registry.register(DistortionEffect.DISTORTION_EFFECT_CODEC, new Identifier(MOD_ID, "static"), StaticDistortionEffect.CODEC);
-		
-		Registry.register(Reverb.SOUND_EFFECTS, new Identifier("overworld"),
-				new SoundEffects(Optional.of(new StaticReverbEffect.Builder().setDecayTime(8.0F).setDensity(0.5F).build()),
-						Optional.empty(),
-						Optional.empty())
-		);
 	}
 	
 }
